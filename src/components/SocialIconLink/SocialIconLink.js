c312c739ffcb09bc38cc26beb50e8969530450a5
Artur Pata <artur.pata@mooncascade.com>
2020-03-24 14:47:25 +0200

    Add fonts from Google API

--- src/components/SocialIconLink/SocialIconLink.js
+++ src/components/SocialIconLink/SocialIconLink.js
@@ -1,40 +1,40 @@
-import React from "react";
-import facebookWhiteIcon from "../../img/social/facebookWhite.svg";
-import twitterWhiteIcon from "../../img/social/twitterWhite.svg";
-import styles from "./SocialIconLink.module.scss";
+import React from 'react'
+import facebookWhiteIcon from '../../img/social/facebookWhite.svg'
+import twitterWhiteIcon from '../../img/social/twitterWhite.svg'
+import styles from './SocialIconLink.module.scss'
 
 const getFacebookIcon = color => {
   switch (color) {
-    case "white":
+    case 'white':
     default:
-      return facebookWhiteIcon;
+      return facebookWhiteIcon
   }
-};
+}
 
 const getTwitterIcon = color => {
   switch (color) {
-    case "white":
+    case 'white':
     default:
-      return twitterWhiteIcon;
+      return twitterWhiteIcon
   }
-};
+}
 
 const getImgPropsForIconType = (type, color) => {
   switch (type) {
-    case "facebook":
-      return { alt: "facebook", src: getFacebookIcon(color) };
-    case "twitter":
-      return { alt: "twitter", src: getTwitterIcon(color) };
+    case 'facebook':
+      return { alt: 'facebook', src: getFacebookIcon(color) }
+    case 'twitter':
+      return { alt: 'twitter', src: getTwitterIcon(color) }
     default:
       return {}
   }
 }
 
-const SocialIconLink = ({ type, href, color = "white" }) => (
+const SocialIconLink = ({ type, href, color = 'white' }) => (
   <a className={styles.noDecoration} href={href}>
     <img
       {...getImgPropsForIconType(type, color)}
-      style={{ width: "20px", height: "20px" }}
+      style={{ width: '20px', height: '20px' }}
     />
   </a>
 )

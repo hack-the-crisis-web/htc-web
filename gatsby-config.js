658c51b5dc89235d22530368a50beb24e2ea4315
Anna Morawska <anna@Annas-MacBook-Pro.local>
2020-03-24 23:26:11 +0200

    reorder plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,16 +13,15 @@ module.exports = {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/content`,
-        name: 'content',
+        path: `${__dirname}/static/img`,
+        name: 'uploads',
       },
     },
     {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/static/img`,
-        name: 'uploads',
+        path: `${__dirname}/src/content`,
+        name: 'content',
       },
     },
     {

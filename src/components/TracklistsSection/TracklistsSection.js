89aecd7ade76d7e158b310ab78300d7320b3cd49
Anna <37595749+anna-morawska@users.noreply.github.com>
2020-03-25 01:12:26 +0200

    Connect section with data source (#21)

--- src/components/TracklistsSection/TracklistsSection.js
+++ src/components/TracklistsSection/TracklistsSection.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classnames from 'classnames'
+import { graphql, StaticQuery } from 'gatsby'
 import '../all.sass'
 import Section from '../Section/Section'
 import SectionTitle from '../SectionTitle/SectionTitle'
@@ -7,36 +8,66 @@ import SectionDescription from '../SectionDescription/SectionDescription'
 import TracklistsCard from '../TracklistsCard/TracklistsCard'
 import styles from './TracklistsSection.module.scss'
 
-const TracklistsSection = () => (
-  <Section className="has-background-success">
-    <div className={classnames(styles.container, 'container has-text-primary')}>
-      <SectionTitle title="Tracklists" />
-      <SectionDescription
-        text="Placeat facere possimus, omnis voluptas assumenda est, omnis dolor
-          repellendus.Mxime placeat facere possimus, omnis voluptas."
-      />
+const TracklistsSection = ({ data }) => {
+  return (
+    <Section className="has-background-success">
       <div
-        className={classnames(
-          styles.cardsWrapper,
-          'columns is-multiline is-mobile is-desktop'
-        )}
+        className={classnames(styles.container, 'container has-text-primary')}
       >
-        {[...Array(12).keys()].map(index => (
-          <div
-            className="column is-half-mobile is-half-tablet is-one-third-desktop"
-            key={index}
-          >
-            <TracklistsCard
-              url="/"
-              title="Education"
-              text="Impedit quo minus id quod maxime Est et expedita distinctio. Nam libero.impedit quo mi"
-              image="https://bulma.io/images/placeholders/128x128.png"
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  </Section>
-)
+        <SectionTitle title="Tracklists" />
+        <SectionDescription
+          text="Placeat facere possimus, omnis voluptas assumenda est, omnis dolor
+            repellendus.Mxime placeat facere possimus, omnis voluptas."
+        />
 
-export default TracklistsSection
+        {data.map(
+          (
+            {
+              node: {
+                frontmatter: { title, description, featuredimage },
+              },
+            },
+            index
+          ) => {
+            return (
+              <div
+                key={index}
+                className="column is-half-mobile is-half-tablet is-one-third-desktop"
+              >
+                <TracklistsCard
+                  url="/"
+                  title={title}
+                  text={description}
+                  image={featuredimage}
+                />
+              </div>
+            )
+          }
+        )}
+      </div>
+    </Section>
+  )
+}
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query TracklistsSection {
+        allMarkdownRemark(
+          filter: { frontmatter: { templateKey: { eq: "tracklists" } } }
+        ) {
+          edges {
+            node {
+              frontmatter {
+                title
+                description
+                featuredimage
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => <TracklistsSection data={data.allMarkdownRemark.edges} />}
+  />
+)

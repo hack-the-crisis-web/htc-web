56a03bee490cb11feb5378c22ea30dbc4636c2bf
arturs <artur.soo@mooncascade.com>
2020-03-24 23:24:33 +0200

    review fixes

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -3,7 +3,7 @@ import classnames from 'classnames'
 import styles from './Section.module.scss'
 
 const Section = ({ className, children }) => (
-  <section className={classnames(styles.section, className)}>
+  <section className={classnames('section', styles.section, className)}>
     {children}
   </section>
 )
--- src/components/TracklistsCard/TracklistsCard.js
+++ src/components/TracklistsCard/TracklistsCard.js
@@ -5,8 +5,8 @@ import styles from './TracklistsCard.module.scss'
 const TracklistsCard = ({ url, text, image, title }) => (
   <Link className={styles.card} to={url}>
     <img alt={title} src={image} />
-    {title && <p className={styles.title}>{title}</p>}
-    {text && <p className={styles.content}>{text}</p>}
+    {!!title && <p className={styles.title}>{title}</p>}
+    {!!text && <p className={styles.content}>{text}</p>}
   </Link>
 )
 

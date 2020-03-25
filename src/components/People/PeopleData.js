fc95f1e0a691a2f06c6590dda50e3c1fad455602
ar7urs <62595705+ar7urs@users.noreply.github.com>
2020-03-25 22:58:25 +0200

    Track page mentors list; If no track leads, then hide track lead section (#54)
    Track page mentors list; If no track leads, then hide track lead section

---
+++ src/components/People/PeopleData.js
@@ -0,0 +1,50 @@
+import { StaticQuery } from 'gatsby'
+import React from 'react'
+
+const filterVisiblePersons = (data, type, hashtag = '') =>
+  data.filter(
+    ({ label, tracklist }) =>
+      label === type &&
+      (type !== 'mentor' || hashtag.includes(tracklist.toLowerCase()))
+  )
+
+const filterData = (data, type, hashtag) => {
+  const people = data.allMarkdownRemark.edges.map(
+    ({ node: { frontmatter } }) => ({
+      ...frontmatter,
+    })
+  )
+  return filterVisiblePersons(people, type, hashtag)
+}
+
+const PeopleData = ({ type, hashtag, children }) => (
+  <StaticQuery
+    query={graphql`
+      query PeopleBlock {
+        allMarkdownRemark(
+          filter: { frontmatter: { templateKey: { eq: "person" } } }
+        ) {
+          edges {
+            node {
+              frontmatter {
+                name
+                surname
+                role
+                photo
+                label
+                tracklist
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data =>
+      React.cloneElement(children, {
+        data: filterData(data, type, hashtag),
+      })
+    }
+  />
+)
+
+export default PeopleData
--- src/components/People/TrackMentors.js
+++ src/components/People/TrackMentors.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import '../all.sass'
-import PeopleBlock from './PeopleBlock'
 import SectionTitle from '../SectionTitle/SectionTitle'
 import SectionDescription from '../SectionDescription/SectionDescription'
 import Section from '../Section/Section'
+import PeopleBlock from './PeopleBlock'
 import PeopleData from './PeopleData'
 
-const TrackLeadsWithData = ({ data, description }) => {
+const TrackMentorsWithData = ({ data, description }) => {
   if (data.length === 0) return null
   return (
     <Section>
-      <SectionTitle title="Track leads" />
+      <SectionTitle title="Mentors" />
       <SectionDescription text={description} />
       <div className="columns is-multiline">
         <PeopleBlock data={data} />
@@ -18,11 +18,10 @@ const TrackLeadsWithData = ({ data, description }) => {
     </Section>
   )
 }
-
-const TrackLeads = ({ description }) => (
-  <PeopleData type="track-lead">
-    <TrackLeadsWithData description={description} />
+const TrackMentors = ({ hashtag, description }) => (
+  <PeopleData type="mentor" hashtag={hashtag}>
+    <TrackMentorsWithData description={description} />
   </PeopleData>
 )
 
-export default TrackLeads
+export default TrackMentors

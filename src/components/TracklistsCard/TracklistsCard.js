0df9a2329799e94b0358eba03e20702c3e41d265
Anna Morawska <anna@Annas-MacBook-Pro.local>
2020-03-25 16:01:10 +0200

    Add hashtags

--- src/components/TracklistsCard/TracklistsCard.js
+++ src/components/TracklistsCard/TracklistsCard.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { Link } from 'gatsby'
 import styles from './TracklistsCard.module.scss'
 
-const TracklistsCard = ({ url, text, image, title }) => (
+const TracklistsCard = ({ url, text, image, title, hashtag }) => (
   <Link className={styles.card} to={url}>
     <img alt={title} src={image} />
     {!!title && <p className={styles.title}>{title}</p>}
     {!!text && <p className={styles.content}>{text}</p>}
+    <div className={styles.footer}>{hashtag}</div>
   </Link>
 )
 

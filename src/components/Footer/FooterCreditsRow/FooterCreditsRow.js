0fcb2cfbe437ffcfe04e2cd96e47ad3f42da850e
Tuuli Haus <tuuli.haus@gmail.com>
2020-03-25 23:33:16 +0200

    Change credits styling

--- src/components/Footer/FooterCreditsRow/FooterCreditsRow.js
+++ src/components/Footer/FooterCreditsRow/FooterCreditsRow.js
@@ -2,29 +2,22 @@ import React from 'react'
 import classNames from 'classnames'
 import styles from './FooterCreditsRow.module.scss'
 import '../../all.sass'
-import mooncascadeLogo from '../../../../static/img/logo-mooncascade.svg'
-import brandmanualLogo from '../../../../static/img/logo-bm.svg'
 
 const FooterCreditsRow = () => (
-  <div className={classNames('has-background-black', 'has-text-grey')}>
+  <div className={classNames('has-background-danger', 'has-text-grey')}>
     <div className={classNames(styles.creditsContainer)}>
       <div className={classNames(styles.creditsInnerContainer)}>
         <div className={classNames(styles.creditsContent)}>
-          <p>designed by:</p>
-
-          <a href="https://thebrandmanual.com/" target="_blank">
-            <img src={brandmanualLogo} alt="Brand manual" />
+          <p>Designed by</p>
+          <a href="https://thebrandmanual.com/" target="_blank" rel="noopener">
+            <p className={classNames(styles.brandManual)}>Brand Manual</p>
           </a>
         </div>
+        <p className={classNames(styles.and)}>&</p>
         <div className={classNames(styles.creditsContent)}>
-          <p>developed by:</p>
-
-          <a
-            className={classNames(styles.imageLink)}
-            href="https://mooncascade.com/"
-            target="_blank"
-          >
-            <img src={mooncascadeLogo} alt="Mooncascade" />
+          <p>Developed by</p>
+          <a href="https://mooncascade.com/" target="_blank" rel="noopener">
+            <p className={classNames(styles.mooncascade)}>Mooncascade</p>
           </a>
         </div>
       </div>

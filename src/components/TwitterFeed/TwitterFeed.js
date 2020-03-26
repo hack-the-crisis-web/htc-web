1b32726c11e8c7a4cb93a497c6ac03d64e9fd0b1
Artur Pata <artur.pata@mooncascade.com>
2020-03-26 17:36:52 +0200

    Fix Twitter feed script not reloading

--- src/components/TwitterFeed/TwitterFeed.js
+++ src/components/TwitterFeed/TwitterFeed.js
@@ -1,31 +1,52 @@
 import React from 'react'
-import Helmet from 'react-helmet'
 import classnames from 'classnames'
 import styles from './TwitterFeed.module.scss'
 
 // DEMO
-// <div class='embedsocial-hashtag' data-ref="0c8c555c7fd05ace23e45f49997f1d246b36fead" ></div><script>(function(d, s, id){var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/cdn/ht.js"; d.getElementsByTagName("head")[0].appendChild(js);}(document, "script", "EmbedSocialHashtagScript"));</script>
+// <div class='embedsocial-hashtag' data-ref="xyz" ></div><script>(function(d, s, id){var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/cdn/ht.js"; d.getElementsByTagName("head")[0].appendChild(js);}(document, "script", "EmbedSocialHashtagScript"));</script>
 
 // const demoProps = {
 //   scriptContent: `(function(d, s, id){var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/cdn/ht.js"; d.getElementsByTagName("head")[0].appendChild(js);})(document, "script", "EmbedSocialHashtagScript")`,
 //   containerConfiguration: {
 //     className: 'embedsocial-hashtag',
-//     dataRef: '0c8c555c7fd05ace23e45f49997f1d246b36fead',
+//     dataRef: 'xyz',
 //   },
 // }
 
-const TwitterFeed = ({ scriptContent, className, dataRef }) => {
-  return (
-    <>
-      <Helmet>
-        <script>{`${scriptContent}`}</script>
-      </Helmet>
+class TwitterFeed extends React.Component {
+  componentDidMount() {
+    if (window && window.document) {
+      const doc = window.document
+      const script = doc.createElement('script')
+      script.innerHTML = this.props.scriptContent
+      doc.getElementsByTagName('head')[0].appendChild(script)
+    }
+  }
+
+  componentWillUnmount() {
+    if (window && window.document) {
+      const iframeId = 'EmbedSocialIFrame'
+      const scriptId = 'EmbedSocialHashtagScript'
+      const lightFrameId = 'EmbedSocialIFrameLightboxCSS'
+      const ids = [iframeId, scriptId, lightFrameId]
+      ids.forEach(id => {
+        const elem = document.getElementById(id)
+        if (elem) {
+          elem.parentNode.removeChild(elem)
+        }
+      })
+    }
+  }
+
+  render() {
+    const { dataRef, className } = this.props
+    return (
       <div
         className={classnames(styles.container, className)}
         data-ref={dataRef}
-      ></div>
-    </>
-  )
+      />
+    )
+  }
 }
 
 export default TwitterFeed

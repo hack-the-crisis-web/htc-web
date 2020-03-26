ed993650303f4b0e86c5544187fd9210b5e27f51
Artur Pata <artur.pata@mooncascade.com>
2020-03-26 11:12:34 +0200

    Fix Twitter title

--- src/components/TwitterFeed/TwitterFeedTitle.js
+++ src/components/TwitterFeed/TwitterFeedTitle.js
@@ -3,7 +3,7 @@ import twitterColorIcon from '../../img/social/twitterColour.svg'
 import styles from './TwitterFeedTitle.module.scss'
 
 const TwitterFeedTitle = ({ children }) => (
-  <div className={styles.centered}>
+  <div className={styles.newBlock}>
     <img alt="Twitter" src={twitterColorIcon} />
     <h2 className={styles.title}>{children}</h2>
   </div>

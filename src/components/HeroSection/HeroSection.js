518fe0b6dc33a03b8606cfa0af18044d090e6187
arturs <artur.soo@mooncascade.com>
2020-03-26 14:38:28 +0200

    Homepage Hero CTA-s changeable in CMS

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,38 +1,61 @@
 import React from 'react'
 import classNames from 'classnames'
 import Button from '../Button/Button'
+import checkIfInternalLink from '../checkIfInternalLink';
 import styles from './HeroSection.module.scss'
 import '../all.sass'
 
-const HeroSection = ({ title, subheading, eventDate, backgroundImage }) => (
-  <section
-    className={classNames('section', styles.heroWrapper)}
-    style={
-      backgroundImage ? { backgroundImage: `url('${backgroundImage}')` } : {}
-    }
-  >
-    <div className={classNames('container', styles.heroContainer)}>
-      <div className="columns">
-        <div className="column is-7 is-offset-1">
-          <h4>{eventDate}</h4>
-          <h1 className="is-family-secondary">{title}</h1>
-          <h3 className="is-family-secondary">{subheading}</h3>
-          <div className={styles.buttonWrapper}>
-            <Button href="#how-it-works">Let's hack</Button>
-            <Button
-              external
-              href="https://theglobalhack.slack.com/"
-              className={styles.joinButton}
-              btnStyle="outline"
-              target="_blank"
-            >
-              Join slack
-            </Button>
+const HeroSection = ({
+  title,
+  subheading,
+  eventDate,
+  backgroundImage,
+  heroCTALeftText,
+  heroCTALeftLink,
+  heroCTARightText,
+  heroCTARightLink,
+}) => {
+  const showLeftButton = !!heroCTALeftLink && !!heroCTALeftText
+  const showRightButton = !!heroCTARightLink && !!heroCTARightText
+  return (
+    <section
+      className={classNames('section', styles.heroWrapper)}
+      style={
+        backgroundImage ? { backgroundImage: `url('${backgroundImage}')` } : {}
+      }
+    >
+      <div className={classNames('container', styles.heroContainer)}>
+        <div className="columns">
+          <div className="column is-7 is-offset-1">
+            <h4>{eventDate}</h4>
+            <h1 className="is-family-secondary">{title}</h1>
+            <h3 className="is-family-secondary">{subheading}</h3>
+            <div className={styles.buttonWrapper}>
+              {showLeftButton && (
+                <Button
+                  external={!checkIfInternalLink(heroCTALeftLink)}
+                  href={heroCTALeftLink}
+                  className={styles.btn}
+                >
+                  {heroCTALeftText}
+                </Button>
+              )}
+              {showRightButton && (
+                <Button
+                  external={!checkIfInternalLink(heroCTARightLink)}
+                  href={heroCTARightLink}
+                  btnStyle="outline"
+                  className={styles.btn}
+                >
+                  {heroCTARightText}
+                </Button>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 
 export default HeroSection

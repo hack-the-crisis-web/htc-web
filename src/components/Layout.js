b94eaf04c8636d4d615b8a51e50868cd28b33ca5
arturs <artur.soo@mooncascade.com>
2020-03-26 12:36:06 +0200

    Mobile bugs, remove hover effect

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,13 +4,26 @@ import Navbar from './Navbar/Navbar'
 import SEO from './SEO/SEO'
 import './all.sass'
 
-const Layout = ({ children, title }) => (
-  <div>
-    <SEO title={title} />
-    <Navbar />
-    <div>{children}</div>
-    <Footer />
-  </div>
-)
+const addDeviceClass = () => {
+  if (typeof window === 'undefined' || !window.document) {
+    return
+  }
+  document.documentElement.classList.add(
+    'ontouchstart' in document.documentElement ? 'touch' : 'no-touch'
+  )
+}
+
+const Layout = ({ children, title }) => {
+  addDeviceClass()
+
+  return (
+    <div>
+      <SEO title={title} />
+      <Navbar />
+      <div>{children}</div>
+      <Footer />
+    </div>
+  )
+}
 
 export default Layout

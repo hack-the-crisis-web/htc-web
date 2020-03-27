24c9c00c93eb471b413a28099f74a9b776d9bd69
Artur Pata <artur.pata@mooncascade.com>
2020-03-27 14:37:00 +0200

    Add timeline to agenda page as well

--- src/templates/agenda-page.js
+++ src/templates/agenda-page.js
@@ -3,40 +3,55 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+import TimelineSection from '../components/TimelineSection/TimelineSection'
 
-export const AgendaPageTemplate = ({ title, content, contentComponent }) => {
+export const AgendaPageTemplate = ({
+  title,
+  content,
+  contentComponent,
+  timelineItems,
+  timelineDescription,
+}) => {
   const PageContent = contentComponent || Content
 
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="columns">
-              <div className="column is-8">
-                <div className="section">
-                  <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                    {title}
-                  </h2>
-                  <PageContent className="content" content={content} />
+    <>
+      <TimelineSection
+        title={'Agenda'}
+        items={timelineItems}
+        description={timelineDescription}
+      />
+      <section className="section section--gradient">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+              <div className="columns">
+                <div className="column is-8">
+                  <div className="section">
+                    <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                      {title}
+                    </h2>
+                    <PageContent className="content" content={content} />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
 
 AgendaPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
+  timelineItems: PropTypes.arrayOf(PropTypes.object),
   content: PropTypes.string,
   contentComponent: PropTypes.func,
 }
 
 const AgendaPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { agendaMarkdown: post, indexMarkdown } = data
 
   return (
     <Layout title="Agenda">
@@ -44,6 +59,8 @@ const AgendaPage = ({ data }) => {
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
+        timelineItems={indexMarkdown.frontmatter.timeline}
+        timelineDescription={indexMarkdown.frontmatter.timelineDescription}
       />
     </Layout>
   )
@@ -57,10 +74,25 @@ export default AgendaPage
 
 export const aboutPageQuery = graphql`
   query AgendaPage($id: String!) {
-    markdownRemark(id: { eq: $id }) {
+    agendaMarkdown: markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
         title
+      }
+    }
+    indexMarkdown: markdownRemark(
+      frontmatter: { templateKey: { eq: "index-page" } }
+    ) {
+      frontmatter {
+        timelineDescription
+        timeline {
+          image {
+            publicURL
+          }
+          title
+          date
+          isActive
+        }
       }
     }
   }

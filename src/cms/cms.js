63100d8c39657a475669109c0afe397ca6dbd130
Anna Morawska <anna@Annas-MacBook-Pro.local>
2020-04-02 11:15:34 +0300

    remove dependency

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,5 @@
 import CMS from 'netlify-cms-app'
-import cms from 'netlify-cms'
+import React from 'react'
 
 import uuid from 'uuid/v4'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
@@ -11,30 +11,41 @@ CMS.registerMediaLibrary(cloudinary)
 /**
  * Create the control widget, this will add a form element to the cms UI
  */
-const IdControl = window.createClass({
-  getInitialState: function() {
-    return {}
-  },
-  componentDidMount: function() {
+
+class IdControl extends React.Component {
+  getInitialState = () => ({})
+
+  componentDidMount = () => {
     if (!this.props.value) {
       this.props.onChange(uuid())
     }
-  },
-  handleChange() {
+  }
+
+  handleChange = () => {
     this.props.onChange(uuid())
-  },
-  render: function() {
-    return window.h('p', null, `${this.props.value}`)
-  },
-})
+  }
 
-const IdPreview = window.createClass({
-  getInitialState: function() {
-    return {}
-  },
-  render: function() {
-    return window.h('p', null, `ID: ${this.props.value}`)
-  },
-})
+  render() {
+    return <p>{this.props.value}</p>
+  }
+}
 
-cms.registerWidget('id', IdControl, IdPreview)
+class IdPreview extends React.Component {
+  getInitialState = () => ({})
+
+  componentDidMount = () => {
+    if (!this.props.value) {
+      this.props.onChange(uuid())
+    }
+  }
+
+  handleChange = () => {
+    this.props.onChange(uuid())
+  }
+
+  render() {
+    return <p>ID: {this.props.value}</p>
+  }
+}
+
+CMS.registerWidget('id', IdControl, IdPreview)

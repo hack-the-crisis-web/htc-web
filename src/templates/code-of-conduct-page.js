f50fc7e5d69892bd6a9e2ca8ae6c1246b56d1718
Artur Pata <artur.pata@mooncascade.com>
2020-04-05 18:40:03 +0300

    Add Code of Conduct

---
+++ src/templates/code-of-conduct-page.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+
+export const CodeOfConductPageTemplate = ({
+  title,
+  content,
+  contentComponent,
+}) => {
+  const PageContent = contentComponent || Content
+
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="columns">
+              <div className="column is-8">
+                <div className="section">
+                  <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                    {title}
+                  </h2>
+                  <PageContent className="content" content={content} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+CodeOfConductPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func,
+}
+
+const CodeOfConductPage = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout title="Code of Conduct">
+      <CodeOfConductPageTemplate
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        content={post.html}
+      />
+    </Layout>
+  )
+}
+
+CodeOfConductPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default CodeOfConductPage
+
+export const codeOfConductPageQuery = graphql`
+  query CodeOfConductPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`

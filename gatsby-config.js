2cf8379a4032e220e55fd0ba063ded356f5a3acf
Anna Morawska <anna@Annas-MacBook-Pro.local>
2020-04-07 18:16:41 +0300

    add blog-images config

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,14 @@ module.exports = {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {
+        path: `${__dirname}/static/blog-images`,
+        name: 'blog-images',
+      },
+    },
+    {
+      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      resolve: 'gatsby-source-filesystem',
+      options: {
         path: `${__dirname}/static/img`,
         name: 'uploads',
       },

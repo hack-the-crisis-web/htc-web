64aaa9aa62facba0b819bb794bb3121a3090c83b
Anna Morawska <anna@Annas-MacBook-Pro.local>
2020-04-07 18:26:27 +0300

    Change config

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/static/blog-images`,
-        name: 'blog-images',
+        name: 'img',
       },
     },
     {

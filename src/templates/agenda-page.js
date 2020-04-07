c1da82667cb34afcdb17330828f412ae7f35fdbb
Anna Morawska <anna@Annas-MacBook-Pro.local>
2020-04-07 09:02:57 +0300

    Add agenda to navbar, remove timeline

--- src/templates/agenda-page.js
+++ src/templates/agenda-page.js
@@ -3,24 +3,12 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
-import TimelineSection from '../components/TimelineSection/TimelineSection'
 
-export const AgendaPageTemplate = ({
-  title,
-  content,
-  contentComponent,
-  timelineItems,
-  timelineDescription,
-}) => {
+export const AgendaPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
     <>
-      <TimelineSection
-        title={'Agenda'}
-        items={timelineItems}
-        description={timelineDescription}
-      />
       <section className="section section--gradient">
         <div className="container">
           <div className="columns">
@@ -44,14 +32,12 @@ export const AgendaPageTemplate = ({
 }
 
 AgendaPageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  timelineItems: PropTypes.arrayOf(PropTypes.object),
   content: PropTypes.string,
   contentComponent: PropTypes.func,
 }
 
 const AgendaPage = ({ data }) => {
-  const { agendaMarkdown: post, indexMarkdown } = data
+  const { agendaMarkdown: post } = data
 
   return (
     <Layout title="Agenda">
@@ -59,8 +45,6 @@ const AgendaPage = ({ data }) => {
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
-        timelineItems={indexMarkdown.frontmatter.timeline}
-        timelineDescription={indexMarkdown.frontmatter.timelineDescription}
       />
     </Layout>
   )
@@ -78,21 +62,6 @@ export const aboutPageQuery = graphql`
       html
       frontmatter {
         title
-      }
-    }
-    indexMarkdown: markdownRemark(
-      frontmatter: { templateKey: { eq: "index-page" } }
-    ) {
-      frontmatter {
-        timelineDescription
-        timeline {
-          image {
-            publicURL
-          }
-          title
-          date
-          isActive
-        }
       }
     }
   }

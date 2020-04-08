804ddf8b39c1692f634f0e08c1d37687c800fc11
Anna <37595749+anna-morawska@users.noreply.github.com>
2020-04-08 05:09:44 +0300

    Add timeline again, with less space (#135)

--- src/templates/agenda-page.js
+++ src/templates/agenda-page.js
@@ -3,19 +3,31 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+import TimelineSection from '../components/TimelineSection/TimelineSection'
 
-export const AgendaPageTemplate = ({ title, content, contentComponent }) => {
+export const AgendaPageTemplate = ({
+  title,
+  content,
+  contentComponent,
+  timelineItems,
+  timelineDescription,
+}) => {
   const PageContent = contentComponent || Content
 
   return (
     <>
       <section className="section section--gradient">
         <div className="container">
+          <TimelineSection
+            title={'Agenda'}
+            items={timelineItems}
+            description={timelineDescription}
+          />
           <div className="columns">
             <div className="column is-10 is-offset-1">
               <div className="columns">
                 <div className="column is-8">
-                  <div className="section">
+                  <div className="section" style={{ paddingTop: 0 }}>
                     <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                       {title}
                     </h2>
@@ -33,11 +45,12 @@ export const AgendaPageTemplate = ({ title, content, contentComponent }) => {
 
 AgendaPageTemplate.propTypes = {
   content: PropTypes.string,
+  timelineItems: PropTypes.arrayOf(PropTypes.object),
   contentComponent: PropTypes.func,
 }
 
 const AgendaPage = ({ data }) => {
-  const { agendaMarkdown: post } = data
+  const { agendaMarkdown: post, indexMarkdown } = data
 
   return (
     <Layout title="Agenda">
@@ -45,6 +58,8 @@ const AgendaPage = ({ data }) => {
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
+        timelineItems={indexMarkdown.frontmatter.timeline}
+        timelineDescription={indexMarkdown.frontmatter.timelineDescription}
       />
     </Layout>
   )
@@ -62,6 +77,21 @@ export const aboutPageQuery = graphql`
       html
       frontmatter {
         title
+      }
+    }
+    indexMarkdown: markdownRemark(
+      frontmatter: { templateKey: { eq: "index-page" } }
+    ) {
+      frontmatter {
+        timelineDescription
+        timeline {
+          image {
+            publicURL
+          }
+          title
+          date
+          isActive
+        }
       }
     }
   }
